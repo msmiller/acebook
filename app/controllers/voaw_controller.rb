950784264155fbd3d385059fb42a4c4a0785ba69
Mark Miller <mmiller@dialedin.com>
2014-07-31 19:00:41 -0700

    !!

--- app/controllers/voaw_controller.rb
+++ app/controllers/voaw_controller.rb
@@ -2,6 +2,7 @@ class VoawController < ApplicationController
 
   def index
     @current_user = current_user
+    @all_users = User.all.sort { |x,y| x.user_info.handle <=> y.user_info.handle }
   end
 
 end

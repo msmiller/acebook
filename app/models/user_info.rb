c8de40d7232d8be04676ae40dc4cbfa158841256
Mark Miller <mmiller@dialedin.com>
2014-08-02 14:32:24 -0700

    ok

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -3,7 +3,7 @@ class UserInfo < ActiveRecord::Base
   belongs_to :user
 
   def self.active_squads
-    UserInfo.all.collect{|u| u.squadron}.uniq.sort
+    UserInfo.all.collect{|u| u.squadron unless u.squadron.blank? }.uniq.sort
   end
 
   def get_icon

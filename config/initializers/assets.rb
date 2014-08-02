9a5aea262040a2731ffd258557de48e783680cab
Mark Miller <mmiller@dialedin.com>
2014-08-02 14:49:26 -0700

    assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,3 @@
 #Rails.application.config.assets.precompile += %w( notice.png )
 #Rails.application.config.assets.precompile += %w( voaw.css )
+Rails.application.config.assets.precompile += %w( gritter-close.png )

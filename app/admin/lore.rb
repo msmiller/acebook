2561ce9d5dc802539e83d02542c73412932c2990
Mark Miller <mark.s.miller@gmail.com>
2014-08-07 20:27:19 -0700

    admin for content

--- app/admin/lore.rb
+++ app/admin/lore.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Lore do
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :user_id, :title, :body, :approved
+  permit_params :user_id, :title, :body, :approved, :game, :kind
   #
   # or
   #
@@ -14,5 +14,17 @@ ActiveAdmin.register Lore do
   #  permitted
   # end
 
+form do |f|
+  f.inputs do
+    f.input :user_id
+    f.input :title
+    f.input :body
+    f.input :game, as: :select, collection: Voaw::Games.keys
+    f.input :kind, as: :select, collection: Lore::KINDS.keys # collect { |x| x[0] }
+    f.input :approved
+    #f.input :gallery_id, as: :select, collection: Gallery.select(:name).uniq
+    #f.input :file
+  end
+end
 
 end
--- app/models/lore.rb
+++ app/models/lore.rb
@@ -2,8 +2,17 @@ class Lore < ActiveRecord::Base
 
   belongs_to :user
 
+  KINDS = {
+    "jargon" => "Jargon",
+    "story" => "Story"
+  }
+
   def lore_params
     params.require(:lore).permit(:title, :body, :user_id)
+  end
+
+  def kind_enum
+   [['Active'],['Pending'],['Banned']]
   end
 
 end

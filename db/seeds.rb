fa490d123e6201e65c494af2ad25e5c8cd7a882c
Mark Miller <mark.s.miller@gmail.com>
2014-08-09 00:02:45 -0700

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,64 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+=begin
+AW Scenarios
+============
+  July/95 Battle Of Germany II
+     "A Clear And Present Hate"
+  May/95  Kursk III
+    "Full Metal Depends(tm)"
+  Feb/95  Munda Weekend
+    "30 Seconds Over Toilet Bowl"
+  Nov/94  Eagle Day II
+    "A Hard Day's Hate"
+  Aug/94  Sicily '43
+    "Hate Is A Many Splendored Thing"
+  Mar/94  Imphal Plain
+     "Bridge On The River Bile"
+  Jan/94  Battle Of Germany
+    "Anticip-hate-tion"
+  Nov/93  Munda
+    "The Silence Of The Sheep"
+  Sep/93  Eagle Day
+    "A Fistful Of Hate"
+  Jul/93  Kursk II
+     "The Stench Of Fear"
+  Jul/92  Indian Ocean
+  May/92  Fortress Rabaul
+  Apr/92  Kursk
+
+WarBirds Scenarios
+  SOLOMONS II  May 1997
+    "Hate Is STILL My Copilot"
+  SOLOMONS (aborted)    February 97
+    "Hate Is My Copilot"
+  KIEL    February 96
+    "Strawberry Dweebs Forever"
+
+Aces High Scenarios
+  Rangoon '42 - Nov/2004
+  "The Sum Of All Hate"
+
+Conventions
+  Air Warrior - Houston - August, 1995
+  Air Warrior - Los Angeles - July 1992
+  Air Warrior - Fallas - September, 1999
+
+News
+  Oct/87 - Air Warrior featured in MacWorld magazine
+
+
+LINGO
+=====
+"HAR!"  Called out when a kill is made. Also used in the phrase: "Har, Maties, bash thar skulls with a belaying pin..."
+"OiNk!" Also called when a kill is made. "OiNk!" is a reference to the Great Piggles Hunt of 1989.
+"burp"  Also called when a kill is made ... usually by Vermin.
+"4Q"  Say it aloud and you'll understand.
+"Blood Dragon"  An A/B17 used as a ground-attack gunship.
+"Blood Pig" The F/A-26 used as an instrument of torture on an enema field.
+"Yoooooo Hoooooo" TC's announcement on channel one that he was out looking for someone ... or some country.
+"Enema" A foe.
+
+=end

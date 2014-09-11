f79d70219417cccf91a1b6df280a3e08d3aefc13
Mark Miller <mark.s.miller@gmail.com>
2014-09-11 00:29:21 -0700

    added search and re-rig menu

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'voaw#index'
+  root 'voaw#roster'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
 
   get 'stats' => 'voaw#stats'
   get 'roster' => 'voaw#roster'
+  get 'index' => 'voaw#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase

6ca08201555456772a11ce252aa90670c0b27782
Mark Miller <mark.s.miller@gmail.com>
2014-09-12 23:43:59 -0700

    better check for unpublished content

--- app/cells/footerlinks_cell.rb
+++ app/cells/footerlinks_cell.rb
@@ -3,9 +3,9 @@ class FooterlinksCell < Cell::Rails
   def show(args)
     @user    = args[:user]
     @numpilots = User.count
-    @num_pending_lores = Lore.where(:approved => false).count
-    @num_pending_timelines = Timeline.where(:approved => false).count
-    @num_pending_links = Link.where(:approved => false).count
+    @num_pending_lores = (Lore.count - Lore.where(:approved => true).count)
+    @num_pending_timelines = (Timeline.count - Timeline.where(:approved => true).count)
+    @num_pending_links = (Link.count - Link.where(:approved => true).count)
     render
   end
 

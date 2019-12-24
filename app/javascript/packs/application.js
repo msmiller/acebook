f6d5dec8e13d1f7716576b14174907ad907fe8be
msmiller <mark.s.miller@gmail.com>
2019-12-23 17:21:49 -0800

    core js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,10 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "controllers"
+
+// Expose jQuery
+// https://stackoverflow.com/questions/47380333/rails-js-erb-templates-no-longer-work-with-webpack
+import 'expose-loader?$!jquery';
+import 'expose-loader?jQuery!jquery';

c0eff83b38fa0b69e21b0396ceed45320e88b9a4
msmiller <mark.s.miller@gmail.com>
2022-06-29 11:07:54 -0700

    footer, fix issues here and there

---
+++ app/components/footer_component.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class FooterComponent < ViewComponent::Base
+
+  def initialize(current_user:)
+    @current_user = current_user
+    @numpilots = User.count
+    @num_pending_lores = (Lore.count - Lore.where(:approved => true).count)
+    @num_pending_timelines = (Timeline.count - Timeline.where(:approved => true).count)
+    @num_pending_links = (Link.count - Link.where(:approved => true).count)
+  end
+
+  def render?
+    true
+  end
+
+
+
+end

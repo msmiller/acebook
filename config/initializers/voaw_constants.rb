5f705f250bdbe49d7c297290fab4c4f37dfe290d
msmiller <mark.s.miller@gmail.com>
2022-07-05 13:51:48 -0700

    propfile

---
+++ config/initializers/voaw_constants.rb
@@ -0,0 +1,61 @@
+Acebook::Games = {
+  "aw" => { 
+    :name => "Air Warrior",
+    :countries => [ "A-Land", "B-Land", "C-Land" ]
+    },
+  "wb" => {
+    :name => "WarBirds",
+    :countries => [ "Red", "Green", "Gold", "Purple" ]
+    },
+  "ah" => {
+    :name => "Aces High",
+    :countries => [ "Rooks", "Knights", "Bishops" ]
+    }
+}
+
+Acebook::Icons = [
+  "sheep",
+  "crosshairs",
+  "a6m",
+  "a26",
+  "b17",
+  "b25",
+  "beaufighter",
+  "f4u",
+  "f6f",
+  "f8f",
+  "fw190",
+  "he162",
+  "hurricane2",
+  "il2m3",
+  "ju88",
+  "ki-100",
+  "la7",
+  "lancaster",
+  "me109",
+  "me262",
+  "mosquito",
+  "p12e",
+  "p38",
+  "p39",
+  "p40",
+  "p47",
+  "p51",
+  "seafury",
+  "spitfire",
+  "wellington",
+  "yak3"
+]
+
+# require 'versionomy'
+# 
+# Acebook::Version = Versionomy.create(
+#   :major => 1,
+#   :minor => 0,
+#   :tiny => 5,
+#   #:tiny2 => 0,
+#   #:release_type => :preview,
+#   #:preview_version => 3
+#   )
+# 
+# Acebook::Date = "12.SEP.2014"

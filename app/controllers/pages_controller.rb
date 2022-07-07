f64bfe21ed1d494cc85b1a859f8fbbd0d5bbf836
msmiller <mark.s.miller@gmail.com>
2022-07-07 14:04:40 -0700

    add demotivators

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,12 @@ class PagesController < ApplicationController
 
   skip_before_action :authenticate_user!
 
+  before_action :set_navbar_active
+
+  def set_navbar_active
+    @navbar_active = 'despair' if params[:id] == 'demotivators'
+  end
+
   private
 
 
